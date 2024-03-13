5b462d27dd1aa0721b5a969fefa4dee7049eb7b7
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-13 13:02:53 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function appendCat(name) {
+    let newCatsArray = cats.slice(); 
+    newCatsArray.push(name);
+    return newCatsArray; 
+}
+function prependCat(name) {
+    let newCatsArray = [name, ...cats]; 
+    return newCatsArray; 
+}
+function removeLastCat() {
+    let newCatsArray = cats.slice(0, -1); 
+    return newCatsArray; 
+} 
+function removeFirstCat() {
+    let newCatsArray = cats.slice(1); 
+    return newCatsArray; 
+}
+
